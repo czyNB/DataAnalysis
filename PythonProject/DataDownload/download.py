bb8bc90d0ae6b6b6483129fefaf36bf7a19091c9
walker <181250102@smail.nju.edu.cn>
2020-05-12 09:35:03 +0800

    initial

--- PythonProject/DataDownload/download.py
+++ PythonProject/DataDownload/download.py
@@ -13,7 +13,7 @@ indexes = list(data)
 # print(cases)
 # 遍历做题信息
 try:
-    os.mkdir('../.data')
+    os.mkdir('../.data/题目分析')
 except FileExistsError:
     pass
 directory = "../.data/"
@@ -47,6 +47,39 @@ for index in indexes:
         zf = zipfile.ZipFile(filename)
         try:
             zf.extractall(path=directory + case_type + '/' + name[:len(name) - 4] + '/' + user_id)
+        except RuntimeError as e:
+            print(e)
+        zf.close()
+        os.remove(filename)
+
+directory = "../.data/用户分析/user_id_"
+for index in indexes:
+    cases = data[index]['cases']
+    case_dir = directory + index
+    try:
+        os.mkdir(case_dir)
+    except FileExistsError:
+        pass
+    for case in cases:
+        print(case["case_id"], case["case_type"])
+        case_type_dir = case_dir + "/" + case["case_type"]
+        try:
+            os.mkdir(case_type_dir)
+        except FileExistsError:
+            pass
+        name = urllib.parse.unquote(os.path.basename(case["case_zip"]))  # 获取文件名，ur1里对中文会urlencode,解个码
+        filename = case_type_dir + "/" + name
+        if "*" in filename:
+            filename = filename.replace("*", "_")
+        print(filename)
+        urllib.request.urlretrieve(case["case_zip"], filename)  # 下载题目包到本地
+        zf = zipfile.ZipFile(filename)
+        try:
+            try:
+                os.mkdir(filename[0:len(filename) - 4])
+            except FileExistsError:
+                pass
+            zf.extractall(filename[0:len(filename) - 4])
         except RuntimeError as e:
             print(e)
         zf.close()
