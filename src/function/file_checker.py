e29772ef1f91cabee928ec6126cbcc1ac73ad6fb
SunriseToSunset <m15256072859@163.com>
2020-06-30 21:14:37 +0800

    feat: 增加判断下载内容是否正确的方法

---
+++ src/function/file_checker.py
@@ -0,0 +1,35 @@
+import os
+import json
+
+
+def topic_checker():
+    # 遍历检查下载内容是否被正确处理
+    f1 = open('../../data/analysis/topic_iterator.json', encoding='utf-8')  # f1为题目分析
+    data = json.loads(f1.read())  # 加载json数据
+    for type in data.keys():
+        for topic in data[type].keys():
+            for user in data[type][topic]:
+                try:
+                    docs = os.listdir('../../data/source/题目分析/' + type + '/' + topic + '/' + user)
+                    assert docs == ['.mooctest', 'blockly.xml', 'main.py', 'properties', 'readme.md']  # 检查目录内容是否为解压内容
+                except AssertionError:
+                    print(type + '/' + topic + '/' + user)
+
+
+def user_checker():
+    # 遍历检查下载内容是否被正确处理
+    f2 = open('../../data/analysis/user_iterator.json', encoding='utf-8')  # f2为用户分析
+    data = json.loads(f2.read())  # 加载json数据
+    for user in data.keys():
+        for type in data[user].keys():
+            for topic in data[user][type]:
+                try:
+                    docs = os.listdir('../../data/source/用户分析/' + user + '/' + type + '/' + topic)
+                    assert docs == ['.mooctest', 'blockly.xml', 'main.py', 'properties', 'readme.md']  # 检查目录内容是否为解压内容
+                except AssertionError:
+                    print(user + '/' + type + '/' + topic)
+
+
+if __name__ == '__main__':
+    topic_checker()
+    user_checker()
