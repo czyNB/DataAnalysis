f9b0d66e5eb59d897aaed1fc139ef6db939eec1b
SunriseToSunset <m15256072859@163.com>
2020-06-30 09:23:15 +0800

    feat: 重新修改项目结构，重写readme

--- src/find_variable.py
+++
@@ -1,6 +0,0 @@
-directory = "../.data/题目分析/查找算法/俄罗斯套娃信封问题_1580656269392/user_id_49823"
-answer_file = open(directory + "/.mooctest/answer.py", "r")
-text = answer_file.readlines()
-words = []
-for sentence in text:
-    words += sentence.strip().split(" ")
