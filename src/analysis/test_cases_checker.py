110339945713f9e184093a35773f07ad820f7598
SunriseToSunset <m15256072859@163.com>
2020-07-02 10:08:21 +0800

    开发日志文件和迭代器类

--- src/analysis/test_cases_checker.py
+++ src/analysis/test_cases_checker.py
@@ -1,15 +1,11 @@
-import json
-import os
+from src.function.iterator import TIterator, UIterator
 
 
 def test_cases_checker():
     result = {}
-    f1 = open('../../data/analysis/topic_iterator.json', encoding='utf-8')  # f1为题目分析
-    data = json.loads(f1.read())  # 加载json数据
-    for type in data.keys():
-        for topic in data[type].keys():
-            for user in data[type][topic]:
-                print()
+    it = UIterator('../../data/analysis/user_iterator.json')
+    while it.next():
+        print(1)
 
 
 if __name__ == '__main__':
