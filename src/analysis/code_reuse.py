34718b3f2ebb7bd91fe4c2de43a3fcd93bcf4c9f
SunriseToSunset <48702884+noGoodnight@users.noreply.github.com>
2020-07-26 09:28:22 +0800

    fix: 解决merge冲突

--- src/analysis/code_reuse.py
+++ src/analysis/code_reuse.py
@@ -1,10 +1,44 @@
 # 代码操作的分析
 import re
+from src.function.iterator import *
+from src.analysis.code_variable import *
 
-def classfy(code: str) -> dict:
+
+def code_reuse(it: UIterator):
+    result = {}
+    current_user = it.get_user()
+    while it.next():
+        classification = classify(it)
+    print('Code Reuse Done!')
+
+
+def classify(it: UIterator) -> dict:
+    variables = get_variable(it)
+    funcs = get_func(it)
+    classes = get_class(it)
+    root = '../../data/source/用户分析/' + it.get_user() + '/' + it.get_type() + '/' + it.get_topic() + '/main.py'
+    code = read_filelines(root)
     result = {
         'advanced': 0,
         'normal': 0,
     }
-    print('Classify Done!')
+
+    i = 0
+    while i < len(code):
+        if code[i] == '':
+            code.remove(code[i])
+            i -= 1
+        elif code[i].startswith('import') or code[i].startswith('def') or code[i].startswith('class') or code[i].startswith('#') or code[i].startswith('return'):
+            code.remove(code[i])
+            i -= 1
+        elif code[i].find('=') != -1 and code[i][code[i].find('=') + 1] != '=':
+            code[i] = code[i][code[i].find('=') + 1:].strip()
+        elif code[i].find('__main__') != -1:
+            code.remove(code[i])
+            i -= 1
+        i += 1
     return result
+
+
+if __name__ == '__main__':
+    code_reuse(getUIterator())
