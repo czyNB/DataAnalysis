0198d92b2f001d40d8a59ead3eff8056a20a53ec
Wang Zhaoxin <181250149@smail.nju.edu.cn>
2020-07-26 16:16:59 +0800

    完善用户命名习惯

--- src/analysis/code_format.py
+++ src/analysis/code_format.py
@@ -20,32 +20,32 @@ def format():
     print("format_over")
 
 
-def evaluate_users():
+def evaluate_user_rmarks():
     # format()
     it = getUIterator()
     it.next()
     content = {}
     while it.now():
-        score_in_codename = evaluate_user(it)
-        # print(check_resonable(variable_list(it)))
+        score_in_codename = evaluate_one(it)
         if it.get_user() not in content.keys():
             content.update({it.get_user(): '%3f' % score_in_codename})
         else:
             break
     # generate_json('../../data/analysis/code_format.json', content)
-    print('    Code format Done!')
+    print(content)
+    return content
 
 
 # 该方法提供单个用户的命名规范程度的接口
-def evaluate_user(it):
+def evaluate_one(it):
     user = it.get_user()
     right_name = 0
     all_name = 0
     flag = True
     user_score = 0
     while flag:
-        all_name += int(get_all(it)[0])
-        right_name += int(get_all(it)[1])
+        all_name += len(variable_list(it))
+        right_name += len(check_reasonable(variable_list(it)))
         if (it.next() and it.get_user() != user):
             flag = False
     # print(all_name)
@@ -53,62 +53,30 @@ def evaluate_user(it):
     # print(user)
     if all_name != 0:
         user_score = right_name * 100 / all_name
+    print(str(user)+' '+str(user_score))
     return user_score
 
 
 # 该题全部命名数量,该题正确命名数量形成一个列表
-def get_all(it):
-    num_all = 0
-    num_right = 0
-
-    content_of_file = read_file('../../data/source/用户分析/' + it.get_user() + '/' + it.get_type()
-                                + '/' + it.get_topic() + '/main.py')
-    content_of_file = list(content_of_file.replace('\n', ' ').split())
-    for element in content_of_file:
-        if check_operator(element):
-            num_all += 1
-    for i in range(0, len(content_of_file)):
-        if check_variable(content_of_file[i]):
-            variable = content_of_file[i - 1]
-            v_list = variable.split('_')
-            for j in v_list:
-                if not j.islower():
-                    break
-            num_right += 1
-        elif check_func(content_of_file[i]):
-            f = content_of_file[i + 1]
-            f_list = f.split('_')
-            for k in f_list:
-                if not k.islower():
-                    break
-            num_right += 1
-        elif check_module(content_of_file[i]):
-            m = content_of_file[i + 1]
-            if m.islower():
-                num_right += 1
-        elif check_class(content_of_file[i]):
-            cl = content_of_file[i + 1]
-            if not cl.isupper() and cl[0].isupper():
-                num_right += 1
-
-    return [num_all, num_right]
-
 
 def check_operator(the_char):
     the_set = ['def', 'class', 'module', '=', '==', '!=', '+=', '-=']
     if the_char in the_set:
         return True
 
-def check_resonable(variables: list) ->bool:
+def check_reasonable(variables: list) ->list:
     check = enchant.Dict("en_US")
-
+    b_list=[]
     if len(variables)>0:
         for element in variables:
             e_list=list(element.split('_'))
             for letter in e_list:
-                if check.check(letter):
-                    return True
-            return False
+                if letter!='':
+                    if check.check(letter):
+                        b_list.append(True)
+                        break
+    return  b_list
+
 
 def check_note(it: UIterator)->int:
     num_of_line=0
