c41071af832fd8bbd8e0fb7a320989889206eb17
SunriseToSunset <48702884+noGoodnight@users.noreply.github.com>
2020-07-26 15:31:37 +0800

    feat: 完成代码复用部分

--- src/analysis/code_reuse.py
+++ src/analysis/code_reuse.py
@@ -6,44 +6,65 @@ from src.analysis.code_variable import *
 
 def code_reuse(it: UIterator):
     result = {}
-    current_user = it.get_user()
     while it.next():
-        classification = classify(it)
+        reuse_score = classify(it)
+        if it.get_user() in result:
+            result[it.get_user()].append(reuse_score)
+        else:
+            result[it.get_user()] = [reuse_score]
+    for item in result.items():
+        result[item[0]] = sum(item[1]) / len(item[1])
+    generate_json('code_reuse.json', result)
     print('Code Reuse Done!')
 
 
-def classify(it: UIterator) -> dict:
+def classify(it: UIterator) -> float:
     variables = variable_list(it)
-    while
-        variables = sorted(variables, key=lambda x: len(x), reverse=True)
+    variables = sorted(variables, key=lambda x: len(x))
+    while len(variables) > 0 and len(variables[0]) == 1:
+        variables = variables[1:]
+        if len(variables) == 0:
+            break
+    variables = list(reversed(variables))
     funcs = func_list(it)
     classes = class_list(it)
     root = '../../data/source/用户分析/' + it.get_user() + '/' + it.get_type() + '/' + it.get_topic() + '/main.py'
     code = read_filelines(root)
     result = {
-        'advanced': 0,
-        'normal': 0,
+        'class_num': 0,
+        'func_num': 0,
+        'my_func': 0,
+        'normal': 0
     }
-    code = clean(clean_variable(clean(clean_init(code)), variables))
-    return result
+    code = clean(clean_init(code))
+    res = clean_class(code, classes)
+    result['class_num'] = res[1]
+    code = clean(res[0])
+    res = clean_func(code, funcs)
+    result['func_num'] = res[1]
+    result['my_func'] = res[2]
+    code = clean(res[0])
+    code = clean(clean_variable(code, variables))
+    result['normal'] = len((' '.join(code)).split(' '))
+    del res, root
+    return (result['class_num'] * 5 + result['my_func'] * 2 + result['func_num'] * 2) / result['normal']
 
 
 def clean_init(code: []) -> []:
     i = 0
     while i < len(code):
-        if code[i] == '':
+        if code[i].startswith('import') or code[i].startswith('def') or code[i].startswith('class') or code[
+            i].startswith('#'):
             code.remove(code[i])
             i -= 1
-        elif code[i].startswith('import') or code[i].startswith('def') or code[i].startswith('class') or code[
-            i].startswith('#') or code[i].startswith('return'):
-            code.remove(code[i])
-            i -= 1
-        elif code[i].find('=') != -1 and code[i][code[i].find('=') + 1] != '=' and code[i][
-            code[i].find('=') - 1] != '!':
+        elif code[i].find('=') != -1 and code[i][code[i].find('=') + 1] == ' ' and code[i][
+            code[i].find('=') - 1] == ' ':
             code[i] = code[i][code[i].find('=') + 1:].strip()
         elif code[i].find('__main__') != -1:
             code.remove(code[i])
             i -= 1
+        elif code[i].startswith('return'):
+            code[i] = ' '.join(code[i].split(' ')[1:])
         i += 1
     return code
 
@@ -51,21 +72,49 @@ def clean_init(code: []) -> []:
 def clean(code: []) -> []:
     i = 0
     while i < len(code):
+        if code[i] == '':
+            code.remove(code[i])
+            i -= 1
         while code[i].endswith(':'):
-            code[i] = code[i][:len(code[i]) - 1]
+            code[i] = ''.join(code[i].split(':'))
         while code[i].startswith('.'):
-            code[i] = code[i][1:]
+            code[i] = ''.join(code[i].split('.'))
+        while code[i].find(',') != -1:
+            code[i] = ''.join(code[i].split(','))
         i += 1
     return code
 
 
 def clean_variable(code: [], variables: []) -> []:
-    i = 0
     content = '\n'.join(code)
     for variable in variables:
         content = ''.join(content.split(variable))
     code = content.split('\n')
     return code
+
+
+def clean_func(code: [], funcs: []) -> []:
+    funcs_num = len(re.findall('[a-zA-z]\(', '\n'.join(code)))
+    my_func_num = 0
+    content = '\n'.join(code)
+    for func in funcs:
+        content = re.split(func + r'[(](.*?)[)]', content)
+        my_func_num += int((len(content) - 1) / 2)
+        content = ''.join(content)
+    code = content.split('\n')
+    return [code, funcs_num, my_func_num]
+
+
+def clean_class(code: [], classes: []) -> []:
+    content = '\n'.join(code)
+    class_num = 0
+    for class_ in classes:
+        class_ = class_[:len(class_) - 1]
+        content = re.split(class_ + r'[(](.*?)[)]', content)
+        class_num += int((len(content) - 1) / 2)
+        content = ''.join(content)
+    code = content.split('\n')
+    return [code, class_num]
 
 
 if __name__ == '__main__':
