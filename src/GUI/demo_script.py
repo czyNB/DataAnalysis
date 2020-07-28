dc65daf2fb1357d3d816746c2465ac1e6f532c6c
Russell Han <49119975+RossHann@users.noreply.github.com>
2020-07-28 10:39:57 +0800

    feat: temp

--- src/GUI/demo_script.py
+++ src/GUI/demo_script.py
@@ -101,7 +101,7 @@ def evaluation(content: str) -> {}:
     f.write(content)
     f.close()
     GUI_format()
-    result = {'variable': 0.0, 'reuse': get_reuse_score(), 'initiation': 0.0, 'all': 0.0}
+    result = {'variable': 0.0, 'reuse': get_reuse_score(), 'initiation': get_variable_score(), 'all': 0.0}
     return result
 
 
@@ -144,4 +144,102 @@ def get_reuse_score() -> float:
 
 
 def get_initiation_score() -> float:
-    return 0
+    content = list(read_file('main.py'))
+    num_of_lines = 0.0
+    num_of_initiation_blocks = 0
+    appeared_variables = []
+    whether_current_block = False
+    for line in content:
+        num_of_lines = num_of_lines + 1
+        line = str(line).replace('==', '+')
+        if '=' not in list(line) and whether_current_block == True:
+            num_of_initiation_blocks = num_of_initiation_blocks + 1
+            whether_current_block = False
+        elif '=' in list(line):
+            the_chars = list(line)
+            is_start = True
+            num_of_blanks = 0
+            for i in the_chars:
+                if i == ' ' and is_start:
+                    num_of_blanks = num_of_blanks + 1
+                elif i != ' ':
+                    break
+            for i in range(num_of_blanks):
+                the_chars.pop(0)
+            current_string = the_chars[0]
+            for i in range(1, len(the_chars)):
+                if not check_operand(current_string + the_chars[i]):
+                    break
+                else:
+                    current_string = current_string + the_chars[i]
+            if current_string not in appeared_variables and not check_special(current_string):
+                whether_current_block = True
+                appeared_variables = appeared_variables + [current_string]
+            elif current_string in appeared_variables:
+                if whether_current_block == True:
+                    num_of_initiation_blocks = num_of_initiation_blocks + 1
+                whether_current_block = False
+            else:
+                whether_current_block = False
+    # print('../../data/source/用户分析/' + it.get_user() + '/' + it.get_type() + '/' + it.get_topic() + '/main.py')
+    # print(num_of_lines, end=' ')
+    # print(num_of_initiation_blocks, end=' ')
+    if num_of_lines == 0.0:
+        return None
+    else:
+        return num_of_initiation_blocks / num_of_lines
+
+
+def check_operand(the_string):
+    no_slash = the_string.split('_')
+    the_str = "".join(no_slash)
+    if len(the_str) == 1:
+        if not the_str.isalpha() and not the_str.isdigit():
+            return False
+    if (re.match('^[a-zA-Z0-9]{0,}', the_str).span() != (0, len(the_str))):
+        return False
+    else:
+        return True
+
+
+def check_special(the_string):
+    the_set = [
+        '',
+        'and',
+        'as',
+        'assert',
+        'break',
+        'class',
+        'continue',
+        'def',
+        'elif',
+        'else',
+        'except',
+        'finally',
+        'for',
+        'from',
+        'if',
+        'import',
+        'in',
+        'is',
+        'lambda',
+        'not',
+        'or',
+        'pass',
+        'raise',
+        'return',
+        'try',
+        'while',
+        'with',
+        'yield',
+        'del',
+        'global',
+        'nonlocal',
+        'True',
+        'False',
+        'None'
+    ]
+    if the_string in the_set:
+        return True
+    else:
+        return False
